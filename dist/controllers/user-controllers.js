39797ab92a05933f7786b6e17a5a28439f5cd06e
Parchello <parchellos@gmail.com>
2024-05-23 14:01:34 +0300

    signup add

--- dist/controllers/user-controllers.js
+++ dist/controllers/user-controllers.js
@@ -1,8 +1,26 @@
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
         return res.status(200).json({ message: "ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(401).json({ message: "Error", cause: error.maeesage });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        await user.save();
+        return res.status(201).json({ message: "user created", email: user.email });
     }
     catch (error) {
         console.log(error);
--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -9,6 +10,27 @@ export const getAllUsers = async (
   try {
     const users = await User.find();
     return res.status(200).json({ message: "ok", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Error", cause: error.maeesage });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    return res.status(201).json({ message: "user created", email: user.email });
   } catch (error) {
     console.log(error);
     return res.status(401).json({ message: "Error", cause: error.maeesage });
