c94886c8421fa2a90317d58a2f9d36db4f0c6396
Parchello <parchellos@gmail.com>
2024-05-23 13:43:36 +0300

    get all add

--- dist/controllers/user-controllers.js
+++ dist/controllers/user-controllers.js
@@ -1,2 +1,12 @@
-export const getAllUsers = () => { };
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(401).json({ message: "Error", cause: error.maeesage });
+    }
+};
 //# sourceMappingURL=user-controllers.js.map
--- src/models/User.ts
+++ src/models/User.ts
@@ -33,4 +33,4 @@ const userSchema = new mongoose.Schema({
   chats: [chatSchema],
 });
 
-export default mongoose.model("User");
+export default mongoose.model("User", userSchema);
